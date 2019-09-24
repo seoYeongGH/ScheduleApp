b314d1d908cf106572f46bc58bd6de07b50c2642
seoyeongGH <syho252@naver.com>
2019-09-24 23:08:00 +0900

    Add UI -Find
    Make findPage design.

---
+++ app/src/main/java/com/example/scheduleapp/FindPage.java
@@ -0,0 +1,14 @@
+package com.example.scheduleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FindPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.find_activity);
+    }
+}
